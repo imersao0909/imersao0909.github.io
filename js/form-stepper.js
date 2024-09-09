002820a09aed081503222e721db70e8f740f3c4e
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-09-09 20:38:47 -0300

    Rodape, contato e cadastro

---
+++ js/form-stepper.js
@@ -0,0 +1,26 @@
+const BTN_NEXT_1 = document.getElementById('next1');
+const BTN_NEXT_2 = document.getElementById('next2');
+
+const DIV_STEP_1 = document.getElementById('step1');
+const DIV_STEP_2 = document.getElementById('step2');
+const DIV_STEP_3 = document.getElementById('step3');
+
+BTN_NEXT_1.addEventListener('click', () => {
+    DIV_STEP_1.classList.add('d-none');
+    DIV_STEP_2.classList.remove('d-none');
+});
+
+BTN_NEXT_2.addEventListener('click', () => {
+    DIV_STEP_2.classList.add('d-none');
+    DIV_STEP_3.classList.remove('d-none');
+});
+
+document.getElementById('prev2').addEventListener('click', () => {
+    DIV_STEP_2.classList.add('d-none');
+    DIV_STEP_1.classList.remove('d-none');
+});
+
+document.getElementById('prev3').addEventListener('click', () => {
+    DIV_STEP_3.classList.add('d-none');
+    DIV_STEP_2.classList.remove('d-none');
+});
